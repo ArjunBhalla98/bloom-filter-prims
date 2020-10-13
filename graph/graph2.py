c484e94e20670fd4c6168a0ccf55e18d383237c1
Ali Abouelatta <ata311@nyu.edu>
2020-10-13 23:45:56 +0200

    Done MST
    Still need function to generate random graph

--- graph/graph2.py
+++ graph/graph2.py
@@ -16,29 +16,38 @@ class Graph2:
         unexplored_nodes = copy.deepcopy(self.graph_arr)
         mst = []
         i = 0
-        temp_arr = []
-        temp_edge_arr = []
-        for every_node_index in explored_nodes:
-            all_edges_for_node = unexplored_nodes[every_node_index].edges
-            """
-            Creating a minimum edge for the MST
-            """
-            min_node_name = min(all_edges_for_node, key=all_edges_for_node.get)
-            name_corrosponding_node = unexplored_nodes[every_node_index].name
-            min_edge = [name_corrosponding_node, min_node_name, all_edges_for_node[min_node_name]]
-            temp_arr.append(all_edges_for_node[min_node_name])
-            temp_edge_arr.append(min_edge)
-        #removing newly explored edges from unexplored Array
-        index_minimum_node_in_explored_nodes = temp_arr.index(min(temp_arr))
-        new_node_name = temp_edge_arr[index_minimum_node_in_explored_nodes][1]
-        corrosponding_node_name = temp_edge_arr[index_minimum_node_in_explored_nodes][0]
-        unexplored_nodes[explored_nodes[index_minimum_node_in_explored_nodes]].edges.pop(new_node_name)
-        index_corrosponding_node = self.graph_name_arr.index(temp_edge_arr[index_minimum_node_in_explored_nodes][1])
-        unexplored_nodes[index_corrosponding_node].edges.pop(corrosponding_node_name)
-        # mst.append(temp_edge_arr[index_minimum_node_in_explored_nodes])
-        #Testing
-        print(unexplored_nodes[0].edges)
-        print(unexplored_nodes[1].edges)
+        while i < len(unexplored_nodes)-1:
+            temp_arr = []
+            temp_edge_arr = []
+            for every_node_index in explored_nodes:
+                all_edges_for_node = unexplored_nodes[every_node_index].edges
+                """
+                Creating a minimum edge for the MST
+                """
+                if len(all_edges_for_node)>0:
+                    min_node_name = min(all_edges_for_node, key=all_edges_for_node.get)
+                    name_corrosponding_node = unexplored_nodes[every_node_index].name
+                    min_edge = [name_corrosponding_node, min_node_name, all_edges_for_node[min_node_name]]
+                    temp_arr.append(all_edges_for_node[min_node_name])
+                    temp_edge_arr.append(min_edge)
+                else:
+                    temp_arr.append(999999999999999)
+                    temp_edge_arr.append(["na","na","-1"])
+            #removing newly explored edges from unexplored Array
+            index_minimum_node_in_explored_nodes = temp_arr.index(min(temp_arr))
+            new_node_name = temp_edge_arr[index_minimum_node_in_explored_nodes][1]
+            corrosponding_node_name = temp_edge_arr[index_minimum_node_in_explored_nodes][0]
+            unexplored_nodes[explored_nodes[index_minimum_node_in_explored_nodes]].edges.pop(new_node_name)
+            index_corrosponding_node = self.graph_name_arr.index(temp_edge_arr[index_minimum_node_in_explored_nodes][1])
+            unexplored_nodes[index_corrosponding_node].edges.pop(corrosponding_node_name)
+            mst.append(temp_edge_arr[index_minimum_node_in_explored_nodes])
+
+            #Appending new node to explored array
+            explored_nodes.append(index_corrosponding_node)
+            
+            #Testing
+            i += 1
+        print("mst is " +str(mst))
         """
         ToDo
         Add explored node in explored nodes array
@@ -54,11 +63,12 @@ class Graph2:
 
 
 
-first_node = Graph2Node("a",{"b":7,"c":3})
-second_node = Graph2Node("b",{"a":7,"c":1})
-third_node = Graph2Node("c",{"b":1,"a":3})
+first_node = Graph2Node("a",{"b":17,"c":3,"d":1})
+second_node = Graph2Node("b",{"a":17,"c":9})
+third_node = Graph2Node("c",{"a":3,"b":9})
+fourth_node = Graph2Node("d",{"a":1})
 
-new_graph = Graph2([first_node,second_node,third_node])
+new_graph = Graph2([first_node,second_node,third_node,fourth_node])
 new_graph.mst()
 
 
