2ad7f17d12a295dd0bc9763360771d349e66c4f1
ArjunBhalla98 <arjunbhallasg@gmail.com>
2020-10-31 07:38:50 +0800

    Bloom Filter MST implementation

--- graph/graph2.py
+++ graph/graph2.py
@@ -1,4 +1,6 @@
-#from https://bradfieldcs.com/algos/graphs/prims-spanning-tree-algorithm/
+# from https://bradfieldcs.com/algos/graphs/prims-spanning-tree-algorithm/
+import bloom_filter
+import sys
 from collections import defaultdict
 import heapq
 
@@ -6,31 +8,60 @@ import heapq
 def minimum_spanning_tree(graph, starting_vertex):
     mst = defaultdict(set)
     visited = set([starting_vertex])
-    edges = [
-        (cost, starting_vertex, to)
-        for to, cost in graph[starting_vertex].items()
-    ]
+    edges = [(cost, starting_vertex, to) for to, cost in graph[starting_vertex].items()]
     heapq.heapify(edges)
+    total_cost = 0
 
     while edges:
         cost, frm, to = heapq.heappop(edges)
         if to not in visited:
             visited.add(to)
+            total_cost += cost
             mst[frm].add(to)
             for to_next, cost in graph[to].items():
                 if to_next not in visited:
                     heapq.heappush(edges, (cost, to, to_next))
 
-    return mst
+    print(f"Set Space: {sys.getsizeof(visited)} Bytes")
+    return mst, cost
+
+
+def bloom_minimum_spanning_tree(graph, starting_vertex):
+    mst = defaultdict(set)
+    visited = bloom_filter.BloomFilter(len(graph.keys()))  # Replace set w/ Bloom
+    edges = [(cost, starting_vertex, to) for to, cost in graph[starting_vertex].items()]
+    heapq.heapify(edges)
+    total_cost = 0
+
+    while edges:
+        cost, frm, to = heapq.heappop(edges)
+        if not visited.probabilistic_contains(to):
+            visited.add(to)
+            total_cost += cost
+            mst[frm].add(to)
+            for to_next, cost in graph[to].items():
+                if not visited.probabilistic_contains(to_next):
+                    heapq.heappush(edges, (cost, to, to_next))
+
+    print(
+        "Bloom Filter Space: " + str(visited.memory_used),
+        "Bloom Filter Filled: " + str(visited.percentage_filled),
+    )
+    return mst, cost
+
 
 example_graph = {
-    'A': {'B': 2, 'C': 3},
-    'B': {'A': 2, 'C': 1, 'D': 1, 'E': 4},
-    'C': {'A': 3, 'B': 1, 'F': 5},
-    'D': {'B': 1, 'E': 1},
-    'E': {'B': 4, 'D': 1, 'F': 1},
-    'F': {'C': 5, 'E': 1, 'G': 1},
-    'G': {'F': 1},
+    "A": {"B": 2, "C": 3},
+    "B": {"A": 2, "C": 1, "D": 1, "E": 4},
+    "C": {"A": 3, "B": 1, "F": 5},
+    "D": {"B": 1, "E": 1},
+    "E": {"B": 4, "D": 1, "F": 1},
+    "F": {"C": 5, "E": 1, "G": 1},
+    "G": {"F": 1},
 }
 
-print(dict(minimum_spanning_tree(example_graph, 'D')))
+set_tree, set_cost = minimum_spanning_tree(example_graph, "D")
+bf_tree, bf_cost = bloom_minimum_spanning_tree(example_graph, "D")
+
+print(f"Set Tree, Set Cost: {set_tree}, {set_cost}")
+print(f"BF Tree, BF Cost: {bf_tree}, {bf_cost}")
