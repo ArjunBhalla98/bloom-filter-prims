85e480ad3020ed7ae966fca60b6d76fbac024286
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-01-05 17:34:19 +0800

    Basic segmentation pipeline + example done

--- segmentation.py
+++ segmentation.py
@@ -1,12 +1,20 @@
-import mst_clustering
 import sklearn
 import numpy as np
 import imageio
 import random
+import matplotlib.pyplot as plt
+from mst_clustering import MSTClustering
 from scipy import ndimage
 
 TEST_FILE_SAVE_PATH = "./results/images/test.jpg"
 HORSE_IMAGE = "./images/BSDS300/images/test/291000.jpg"
+
+"""
+Next steps:
+
+1. Instead of having all the points connected to each other, they should only be connected to their (max 8) direct neighbours
+2. implement a full scale / try to scale this up without segfault to BSDS image sets
+"""
 
 
 def image_to_matrix(image):
@@ -20,10 +28,27 @@ def image_to_matrix(image):
 if __name__ == "__main__":
     img = imageio.imread(HORSE_IMAGE)
     print("Image Loaded")
-    for i in range(len(img)):
-        for j in range(len(img[0])):
-            img[i][j][0] = random.randint(0, 255)
+    # img = np.mean(img, 2)
+    img = img[201:, 321:, :]
 
+    a = len(img)
+    b = len(img[0])
+    img = np.reshape(img, (len(img) * len(img[0]), 3))
+    print("Image reshaped")
+    model = sklearn
+    model = MSTClustering(cutoff_scale=4.3, approximate=False)
+    print("Model init")
+    labels = model.fit_predict(img)
+    print("Labelled")
+
+    colours = [[255, 255, 255], [0, 0, 0], [255, 0, 0], [0, 255, 0], [0, 0, 255]]
+
+    for i in range(len(img)):
+        img[i] = colours[labels[i] % len(colours)]
+
+    img = np.reshape(img, (a, b, 3))
+
+    # print(labels)
     imageio.imsave(TEST_FILE_SAVE_PATH, img)
-    print("Image Saved")
+    # print("Image Saved")
 
